edbbe3b0953b965b9537860e64829756c6ce03fd
Matthew Charland <ny.matt.charland3@gmail.com>
2019-11-03 05:04:56 -0500

    speech to text

--- app/src/main/java/com/example/vibecheck/MainActivity.java
+++ app/src/main/java/com/example/vibecheck/MainActivity.java
@@ -2,20 +2,24 @@ package com.example.vibecheck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.os.Vibrator;
+import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     Vibrator vibrator;
-    TextView textResults;
+    TextView textResults = (TextView)findViewById(R.id.editText);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -179,10 +183,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-//    public void getSpeechInput(View view){
-//        Intent intent = new Intent();
-//
-//    }
+    public void getSpeechInput(View view){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, 10);
+        }else{
+            Toast.makeText(this, "Device doesn't support speech input", Toast.LENGTH_SHORT).show();
+        }
+        
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case 10:
+                if (requestCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    textResults.setText(result.get(0));
+                }
+                break;
+        }
+    }
 
 
 
